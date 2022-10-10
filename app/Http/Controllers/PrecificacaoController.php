600201bc4e6e2f41ce8786bf63efa64e83263deb
Mariotti <prog42@ixcsoft.com.br>
2022-10-10 09:10:55 -0300

    calculo precificação

--- app/Http/Controllers/PrecificacaoController.php
+++ app/Http/Controllers/PrecificacaoController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pagamento;
+use App\Models\Recebimento;
 use Illuminate\Http\Request;
 
 class PrecificacaoController extends Controller
 {
-    public function get(Request $request)
+    public function get(Pagamento $Pagamento, Recebimento $Recebimento)
     {
+        $totalFixo = Pagamento::whereRaw('data_pagamento > DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH) AND tipo_custo = \'F\'')->sum('valor');
+        $totalVariavel = Pagamento::whereRaw('data_pagamento > DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH) AND tipo_custo = \'V\'')->sum('valor');
+        $numeroClientes = Recebimento::whereRaw('data_recebimento > DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH)')->count();
 
+        $diferencaTotais = $totalFixo - $totalVariavel;
+        $valorMedio = abs($diferencaTotais / $numeroClientes);
+        $numeroMinimoClientes = abs($diferencaTotais / $valorMedio);
+
+        return [
+            'valorMedio' => $valorMedio,
+            'numeroMinimoClientes' => $numeroMinimoClientes
+        ];
     }
 }
