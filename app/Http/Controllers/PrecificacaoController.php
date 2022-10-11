acd225e49b8ed1d0257633d29a0ae74fbdcd7a1b
JP Mariotti <mariotti660@gmail.com>
2022-10-10 21:23:27 -0300

    ajuste calculos

--- app/Http/Controllers/PrecificacaoController.php
+++ app/Http/Controllers/PrecificacaoController.php
@@ -10,9 +10,9 @@ class PrecificacaoController extends Controller
 {
     public function get(Pagamento $Pagamento, Recebimento $Recebimento)
     {
-        $totalFixo = Pagamento::whereRaw('data_pagamento > DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH) AND tipo_custo = \'F\'')->sum('valor');
-        $totalVariavel = Pagamento::whereRaw('data_pagamento > DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH) AND tipo_custo = \'V\'')->sum('valor');
-        $numeroClientes = Recebimento::whereRaw('data_recebimento > DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH)')->count();
+        $totalFixo = Pagamento::whereRaw('data_pagamento > DATE_SUB((SELECT MAX(p2.data_pagamento) FROM pagamentos p2), INTERVAL 1 MONTH) AND tipo_custo = \'F\'')->sum('valor');
+        $totalVariavel = Pagamento::whereRaw('data_pagamento > DATE_SUB((SELECT MAX(p2.data_pagamento) FROM pagamentos p2), INTERVAL 1 MONTH) AND tipo_custo = \'V\'')->sum('valor');
+        $numeroClientes = Recebimento::whereRaw('data_recebimento > DATE_SUB((SELECT MAX(r2.data_recebimento) FROM pagamentos r2), INTERVAL 1 MONTH)')->count();
 
         $diferencaTotais = $totalFixo - $totalVariavel;
         $valorMedio = abs($diferencaTotais / $numeroClientes);
