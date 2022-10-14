9b1223972dd674614815f6d8f577f97859b77b10
Mariotti <prog42@ixcsoft.com.br>
2022-10-14 17:19:42 -0300

    container login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,34 +5,36 @@
 @section('fab')@endsection
 
 @section('content')
-    <div class="col s12 row-login">
-        <h2 class="header center">Bem Vindo</h2>
-        <div class="card horizontal">
-            <div class="card-stacked">
-                <form action="{{route('login.make')}}" method="POST">
-                    @csrf
-                    <div class="card-content">
-                        <div class='row'>
-                            <div class="input-field col s12">
-                                <input id="email" name="email" type="text" required autocomplete="off">
-                                <label for="email">Login</label>
+    <div class="container">
+        <div class="col s12 row-login">
+            <h2 class="header center">Bem Vindo</h2>
+            <div class="card horizontal">
+                <div class="card-stacked">
+                    <form action="{{route('login.make')}}" method="POST">
+                        @csrf
+                        <div class="card-content">
+                            <div class='row'>
+                                <div class="input-field col s12">
+                                    <input id="email" name="email" type="text" required autocomplete="off">
+                                    <label for="email">Login</label>
+                                </div>
+                            </div>
+                            <div class='row'>
+                                <div class="input-field col s12">
+                                    <input id="password" name="password" type="password" required autocomplete="off">
+                                    <label for="password">Senha</label>
+                                </div>
                             </div>
                         </div>
-                        <div class='row'>
-                            <div class="input-field col s12">
-                                <input id="password" name="password" type="password" required autocomplete="off">
-                                <label for="password">Senha</label>
+                        <div class="card-action">
+                            <div class='row'>
+                                <button type='submit' name='btn_login' class='indigo darken-4 col s12 btn btn-large waves-effect'>
+                                    Fazer Login <i class="material-icons right">send</i>
+                                </button>
                             </div>
                         </div>
-                    </div>
-                    <div class="card-action">
-                        <div class='row'>
-                            <button type='submit' name='btn_login' class='indigo darken-4 col s12 btn btn-large waves-effect'>
-                                Fazer Login <i class="material-icons right">send</i>
-                            </button>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
