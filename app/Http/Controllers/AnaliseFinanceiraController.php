5449a58769219d7a0936127f67df6a6c173e7fdb
JP Mariotti <mariotti660@gmail.com>
2022-10-13 22:11:17 -0300

    contas

--- app/Http/Controllers/AnaliseFinanceiraController.php
+++ app/Http/Controllers/AnaliseFinanceiraController.php
@@ -9,9 +9,9 @@ class AnaliseFinanceiraController extends Controller
 {
     public function get()
     {
-        $totalVariavel = Pagamento::whereRaw('data_vencimento > DATE_SUB((SELECT MAX(p2.data_pagamento) FROM pagamentos p2 WHERE tipo_custo = \'V\'), INTERVAL 30 DAY) AND tipo_custo = \'V\'')->sum('valor');
-        $receitaTotal = Recebimento::whereRaw('data_vencimento > DATE_SUB((SELECT MAX(r2.data_recebimento) FROM recebimentos r2), INTERVAL 30 DAY)')->sum('valor');
-        $totalFixo = Pagamento::whereRaw('data_vencimento > DATE_SUB((SELECT MAX(p2.data_pagamento) FROM pagamentos p2 WHERE tipo_custo = \'F\'), INTERVAL 30 DAY) AND tipo_custo = \'F\'')->sum('valor');
+        $receitaTotal = Recebimento::whereRaw('data_vencimento >= DATE_SUB((SELECT MAX(r2.data_recebimento) FROM recebimentos r2), INTERVAL 30 DAY)')->sum('valor');
+        $totalVariavel = Pagamento::whereRaw('data_vencimento >= DATE_SUB((SELECT MAX(p2.data_pagamento) FROM pagamentos p2 WHERE tipo_custo = \'V\'), INTERVAL 30 DAY) AND tipo_custo = \'V\'')->sum('valor');
+        $totalFixo = Pagamento::whereRaw('data_vencimento >= DATE_SUB((SELECT MAX(p2.data_pagamento) FROM pagamentos p2 WHERE tipo_custo = \'F\'), INTERVAL 30 DAY) AND tipo_custo = \'F\'')->sum('valor');
 
         $margemContribuicao = $receitaTotal - $totalVariavel;
         $pontoEquilibrio = $totalFixo / $margemContribuicao;
--- app/Http/Controllers/PrecificacaoController.php
+++ app/Http/Controllers/PrecificacaoController.php
@@ -4,15 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Pagamento;
 use App\Models\Recebimento;
-use Illuminate\Http\Request;
 
 class PrecificacaoController extends Controller
 {
-    public function get(Pagamento $Pagamento, Recebimento $Recebimento)
+    public function get()
     {
-        $totalFixo = Pagamento::whereRaw('data_pagamento > DATE_SUB((SELECT MAX(p2.data_pagamento) FROM pagamentos p2), INTERVAL 1 MONTH) AND tipo_custo = \'F\'')->sum('valor');
-        $totalVariavel = Pagamento::whereRaw('data_pagamento > DATE_SUB((SELECT MAX(p2.data_pagamento) FROM pagamentos p2), INTERVAL 1 MONTH) AND tipo_custo = \'V\'')->sum('valor');
-        $numeroClientes = Recebimento::whereRaw('data_recebimento > DATE_SUB((SELECT MAX(r2.data_recebimento) FROM recebimentos r2), INTERVAL 1 MONTH)')->count();
+        $totalVariavel = Pagamento::whereRaw('data_vencimento >= DATE_SUB((SELECT MAX(p2.data_pagamento) FROM pagamentos p2 WHERE tipo_custo = \'V\'), INTERVAL 30 DAY) AND tipo_custo = \'V\'')->sum('valor');
+        $totalFixo = Pagamento::whereRaw('data_vencimento >= DATE_SUB((SELECT MAX(p2.data_pagamento) FROM pagamentos p2 WHERE tipo_custo = \'F\'), INTERVAL 30 DAY) AND tipo_custo = \'F\'')->sum('valor');
+        $numeroClientes = Recebimento::whereRaw('data_recebimento >= DATE_SUB((SELECT MAX(r2.data_recebimento) FROM recebimentos r2), INTERVAL 30 DAY)')->count();
 
         $diferencaTotais = $totalFixo - $totalVariavel;
         $valorMedio = abs($diferencaTotais / $numeroClientes);
